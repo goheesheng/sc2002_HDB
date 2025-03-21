bf044db73074101e3ee6b3a8c26947e4fe88fa62
Goh Ee Sheng <goheesheng@hotmail.com>
2025-03-21 16:08:34 +0800

    Base Project template

--- src/project/Project.java
+++ src/project/Project.java
@@ -1,78 +1,94 @@
 package project;
 
+import user.HDBManager;
+import user.HDBOfficer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public class Project {
+    private String projectId;
     private String projectName;
     private String neighborhood;
-    private int availableUnits2Room;
-    private int availableUnits3Room;
-    private String applicationOpeningDate;
-    private String applicationClosingDate;
+    private Map<FlatType, Integer> flatTypes;
+    private Date applicationOpeningDate;
+    private Date applicationClosingDate;
+    private HDBManager managerInCharge;
+    private int availableOfficerSlots;
     private boolean visibility;
-    private int availableHDBOfficerSlots;
+    private List<HDBOfficer> officers;
+    private List<Application> applications;
+    private List<Enquiry> enquiries;
 
-    public Project(String projectName, String neighborhood, int availableUnits2Room, 
-                   int availableUnits3Room, String applicationOpeningDate, 
-                   String applicationClosingDate, boolean visibility, 
-                   int availableHDBOfficerSlots) {
+    public Project(String projectId, String projectName, String neighborhood, 
+                  Map<FlatType, Integer> flatTypes, Date applicationOpeningDate, 
+                  Date applicationClosingDate, HDBManager managerInCharge, 
+                  int availableOfficerSlots) {
+        this.projectId = projectId;
         this.projectName = projectName;
         this.neighborhood = neighborhood;
-        this.availableUnits2Room = availableUnits2Room;
-        this.availableUnits3Room = availableUnits3Room;
+        this.flatTypes = flatTypes;
         this.applicationOpeningDate = applicationOpeningDate;
         this.applicationClosingDate = applicationClosingDate;
-        this.visibility = visibility;
-        this.availableHDBOfficerSlots = availableHDBOfficerSlots;
+        this.managerInCharge = managerInCharge;
+        this.availableOfficerSlots = availableOfficerSlots;
+        this.visibility = false;
+        this.officers = new ArrayList<>();
+        this.applications = new ArrayList<>();
+        this.enquiries = new ArrayList<>();
     }
 
+    public int getRemainingFlats(FlatType flatType) {
+        return flatTypes.getOrDefault(flatType, 0);
+    }
+
+    public boolean isEligibleForUser(user.User user) {
+        if (user.getMaritalStatus().equals("SINGLE") && user.getAge() >= 35) {
+            return getRemainingFlats(FlatType.TWO_ROOM) > 0;
+        } else if (user.getMaritalStatus().equals("MARRIED") && user.getAge() >= 21) {
+            return getRemainingFlats(FlatType.TWO_ROOM) > 0 || 
+                   getRemainingFlats(FlatType.THREE_ROOM) > 0;
+        }
+        return false;
+    }
+
+    public boolean isWithinApplicationPeriod() {
+        Date now = new Date();
+        return now.after(applicationOpeningDate) && now.before(applicationClosingDate);
+    }
+
+    public void addEnquiry(Enquiry enquiry) {
+        enquiries.add(enquiry);
+    }
+
+    public void addApplication(Application application) {
+        applications.add(application);
+    }
+
+    // Getters and setters
+    public List<HDBOfficer> getOfficers() { // need check 
+        return new ArrayList<>(officers);
+    }
+    
+    public String getProjectId() {
+        return projectId;
+    }
+    public String getneighborhood() {
+        return neighborhood;
+    }
+    public int getavailableOfficerSlots() {
+        return availableOfficerSlots;
+    }
     public String getProjectName() {
         return projectName;
     }
 
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
+    public HDBManager getManagerInCharge() {
+        return managerInCharge;
     }
 
-    public String getNeighborhood() {
-        return neighborhood;
-    }
-
-    public void setNeighborhood(String neighborhood) {
-        this.neighborhood = neighborhood;
-    }
-
-    public int getAvailableUnits2Room() {
-        return availableUnits2Room;
-    }
-
-    public void setAvailableUnits2Room(int availableUnits2Room) {
-        this.availableUnits2Room = availableUnits2Room;
-    }
-
-    public int getAvailableUnits3Room() {
-        return availableUnits3Room;
-    }
-
-    public void setAvailableUnits3Room(int availableUnits3Room) {
-        this.availableUnits3Room = availableUnits3Room;
-    }
-
-    public String getApplicationOpeningDate() {
-        return applicationOpeningDate;
-    }
-
-    public void setApplicationOpeningDate(String applicationOpeningDate) {
-        this.applicationOpeningDate = applicationOpeningDate;
-    }
-
-    public String getApplicationClosingDate() {
-        return applicationClosingDate;
-    }
-
-    public void setApplicationClosingDate(String applicationClosingDate) {
-        this.applicationClosingDate = applicationClosingDate;
-    }
-
-    public boolean isVisibility() {
+    public boolean isVisible() {
         return visibility;
     }
 
@@ -80,17 +96,4 @@ public class Project {
         this.visibility = visibility;
     }
 
-    public int getAvailableHDBOfficerSlots() {
-        return availableHDBOfficerSlots;
-    }
-
-    public void setAvailableHDBOfficerSlots(int availableHDBOfficerSlots) {
-        this.availableHDBOfficerSlots = availableHDBOfficerSlots;
-    }
-
-    public String viewDetails() {
-        return "Project: " + projectName + " in " + neighborhood + "\n" +
-               "Available 2-room flats: " + availableUnits2Room + "\n" +
-               "Available 3-room flats: " + availableUnits3Room;
-    }
 }
