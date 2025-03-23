520e263b7a4b97e5cbbe894f032c83939bb378f3
Goh Ee Sheng <goheesheng@hotmail.com>
2025-03-23 10:05:14 +0800

    ADd Javadoc for FlatType

--- src/project/FlatType.java
+++ src/project/FlatType.java
@@ -1,6 +1,17 @@
-package project;
-
+/**
+ * Represents the types of flats available in BTO projects.
+ * 
+ * @author SC2002 G Team
+ * @version 1.0
+ * @since 2025-03-16
+ */
 public enum FlatType {
+    /**
+     * A two-room flat.
+     */
     TWO_ROOM,
+    /**
+     * A three-room flat.
+     */
     THREE_ROOM
 }
