6097c0b254d48f90ecd4f887f8447d564f4e2ce4
Goh Ee Sheng <goheesheng@hotmail.com>
2025-03-23 10:27:19 +0800

    Added Javadoc for Applicant

--- src/user/Applicant.java
+++ src/user/Applicant.java
@@ -5,15 +5,37 @@ import project.Application;
 import project.FlatType;
 import status.ApplicationStatus;
 
+/**
+ * Represents an applicant for BTO housing projects.
+ * Applicants can apply for projects, view their applications, and book flats.
+ * 
+ * @author SC2002 G Team
+ * @version 1.0
+ * @since 2025-03-16
+ */
 public class Applicant extends User {
     private Project appliedProject;
     private ApplicationStatus applicationStatus;
     private FlatType bookedFlatType;
 
+    /**
+     * Creates a new Applicant with the specified details.
+     * 
+     * @param nric The NRIC of this applicant
+     * @param password The password for authentication
+     * @param age The age of this applicant
+     * @param maritalStatus The marital status of this applicant
+     */
     public Applicant(String nric, String password, int age, String maritalStatus) {
         super(nric, password, age, maritalStatus);
     }
 
+    /**
+     * Applies for a BTO project if the applicant is eligible.
+     * 
+     * @param project The project to apply for
+     * @return true if the application was successful, false otherwise
+     */
     public boolean applyForProject(Project project) {
         if (appliedProject == null && project.isEligibleForUser(this)) {
             appliedProject = project;
@@ -30,6 +52,11 @@ public class Applicant extends User {
         return false;
     }
 
+    /**
+     * Retrieves the application submitted by this applicant.
+     * 
+     * @return The application, or null if no application exists
+     */
     public Application viewApplication() {
         if (appliedProject != null) {
             for (Application app : appliedProject.getApplications()) {
@@ -41,6 +68,11 @@ public class Applicant extends User {
         return null;
     }
 
+    /**
+     * Requests withdrawal of the applicant's application.
+     * 
+     * @return true if the withdrawal request was successful, false otherwise
+     */
     public boolean requestWithdrawal() {
         if (appliedProject != null) {
             Application application = viewApplication();
@@ -50,6 +82,14 @@ public class Applicant extends User {
         }
         return false;
     }
+
+    /**
+     * Books a flat in a project after a successful application.
+     * 
+     * @param project The project to book a flat in
+     * @param flatType The type of flat to book
+     * @return true if the booking was successful, false otherwise
+     */
     public boolean bookFlat(Project project, FlatType flatType) {
         if (applicationStatus == ApplicationStatus.SUCCESSFUL && appliedProject == project) {
             bookedFlatType = flatType;
@@ -64,7 +104,11 @@ public class Applicant extends User {
     }
     
        
-
+    /**
+     * Determines the eligible flat type for this applicant based on marital status.
+     * 
+     * @return The eligible flat type
+     */
     private FlatType getEligibleFlatType() {
         if (getMaritalStatus().equals("SINGLE")) {
             return FlatType.TWO_ROOM;
@@ -74,19 +118,38 @@ public class Applicant extends User {
         }
     }
 
-    // Getters and setters
+    /**
+     * Gets the project this applicant has applied for.
+     * 
+     * @return The applied project, or null if no application exists
+     */
     public Project getAppliedProject() {
         return appliedProject;
     }
-    //@audit add UML
+    
+    /**
+     * Gets the type of flat booked by this applicant.
+     * 
+     * @return The booked flat type, or null if no flat is booked
+     */
     public FlatType getbookedFlatType() {
         return bookedFlatType;
     }
-
+    
+    /**
+     * Gets the current status of this applicant's application.
+     * 
+     * @return The application status
+     */
     public ApplicationStatus getApplicationStatus() {
         return applicationStatus;
     }
-
+    
+    /**
+     * Sets the status of this applicant's application.
+     * 
+     * @param status The new application status
+     */
     public void setApplicationStatus(ApplicationStatus status) {
         this.applicationStatus = status;
     }
